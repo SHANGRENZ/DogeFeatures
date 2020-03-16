fd04f9e750eccbacc9a033daae6adbc484b16558
naiveDoge <zhoushangren@outlook.com>
2020-03-16 23:09:20 +0800

    bug fix.

--- DogeFeatures/src/cn/hotdoge/dogefeatures/EventEatFood.java
+++ DogeFeatures/src/cn/hotdoge/dogefeatures/EventEatFood.java
@@ -1,5 +1,6 @@
 package cn.hotdoge.dogefeatures;
 
+import java.util.Collection;
 import java.util.Date;
 
 import org.bukkit.Bukkit;
@@ -19,31 +20,43 @@ import org.bukkit.potion.PotionEffectType;
 
 public class EventEatFood implements Listener {
 	
+	Collection<PotionEffect> activeEffects;
 	@EventHandler
 	public void onPlayerItemConsume(PlayerItemConsumeEvent e) {
 		Player p = e.getPlayer();
 		if(new ItemStack(Material.PUFFERFISH).isSimilar(e.getItem()) && DogeFeatures.getPlugin().getConfig().getBoolean("featureSettings.pufferfish")) {
-			e.setCancelled(true);
-			p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_BURP, 0.5F, 1F);
+			activeEffects = p.getActivePotionEffects();
+			
 			p.sendTitle(ChatColor.YELLOW + "干了 奥里给!", "干就完事了", 10, 70, 20);
-			p.setFoodLevel(p.getFoodLevel() < 20 ? p.getFoodLevel() + 1 : p.getFoodLevel());
-			
-			p.getInventory().removeItem(new ItemStack(Material.PUFFERFISH, 1));
-			p.updateInventory();
-			
-			p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 100, 0));
-			p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 120, 1));
-			p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 140, 0));
 			
 			Bukkit.getScheduler().runTaskLater(DogeFeatures.getPlugin(), new Runnable() {
 				
 				@Override
 				public void run() {
+					activeEffects = p.getActivePotionEffects();
+					
+					for(PotionEffect effect:activeEffects) {
+						p.removePotionEffect(effect.getType());
+					}
+					
+					p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 100, 0));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 120, 1));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 140, 0));
+				}
+			}, 5);
+			Bukkit.getScheduler().runTaskLater(DogeFeatures.getPlugin(), new Runnable() {
+				
+				@Override
+				public void run() {
+					for(PotionEffect effect:activeEffects) {
+						p.addPotionEffect(effect);
+					}
+					
 					//p.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 500, 0));
-					p.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 3000, 7));
-					p.addPotionEffect(new PotionEffect(PotionEffectType.HUNGER, 2200, 5));
-					p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 1400, 1));
-					p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 2000, 1));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 3200, 3));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.HUNGER, 2500, 2));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 2000, 0));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 1400, 1));
 				}
 			}, 180);
 			
