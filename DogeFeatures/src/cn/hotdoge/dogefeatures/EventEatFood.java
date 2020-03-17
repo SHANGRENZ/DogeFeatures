2cc7da1bdedd3452b09b4a253a3cabccd81fde2b
naiveDoge <zhoushangren@outlook.com>
2020-03-17 10:07:30 +0800

    pufferfish fix.

--- DogeFeatures/src/cn/hotdoge/dogefeatures/EventEatFood.java
+++ DogeFeatures/src/cn/hotdoge/dogefeatures/EventEatFood.java
@@ -1,5 +1,8 @@
 package cn.hotdoge.dogefeatures;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 
@@ -25,9 +28,19 @@ public class EventEatFood implements Listener {
 	public void onPlayerItemConsume(PlayerItemConsumeEvent e) {
 		Player p = e.getPlayer();
 		if(new ItemStack(Material.PUFFERFISH).isSimilar(e.getItem()) && DogeFeatures.getPlugin().getConfig().getBoolean("featureSettings.pufferfish")) {
+			try {
+				if(p.getMetadata("isPufferfishJustEat").get(0).asBoolean()) {
+					e.setCancelled(true);
+					p.sendTitle(ChatColor.YELLOW + "口区——", "你短时间不能食用太多的奥利给", 5, 20, 5);
+					return;
+				}
+			} catch (Exception e2) {
+				//To do nothing
+			}
+			
 			activeEffects = p.getActivePotionEffects();
 			
-			p.sendTitle(ChatColor.YELLOW + "干了 奥里给!", "干就完事了", 10, 70, 20);
+			p.sendTitle(ChatColor.YELLOW + "干了 奥里给!", "干就完事了", 10, 70, 10);
 			
 			Bukkit.getScheduler().runTaskLater(DogeFeatures.getPlugin(), new Runnable() {
 				
@@ -42,23 +55,37 @@ public class EventEatFood implements Listener {
 					p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 100, 0));
 					p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 120, 1));
 					p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 140, 0));
-				}
-			}, 5);
-			Bukkit.getScheduler().runTaskLater(DogeFeatures.getPlugin(), new Runnable() {
-				
-				@Override
-				public void run() {
-					for(PotionEffect effect:activeEffects) {
-						p.addPotionEffect(effect);
-					}
 					
-					//p.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 500, 0));
-					p.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 3200, 3));
-					p.addPotionEffect(new PotionEffect(PotionEffectType.HUNGER, 2500, 2));
-					p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 2000, 0));
-					p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 1400, 1));
+					p.setMetadata("isPufferfishJustEat", new FixedMetadataValue(DogeFeatures.getPlugin(), true));
+					Bukkit.getScheduler().runTaskLater(DogeFeatures.getPlugin(), new Runnable() {
+						
+						@Override
+						public void run() {
+							for(PotionEffect effect:activeEffects) {
+								if(!Arrays.asList(PotionEffectType.POISON, PotionEffectType.HUNGER, PotionEffectType.CONFUSION, PotionEffectType.BLINDNESS).contains(effect.getType())) {
+									p.addPotionEffect(effect);
+								}
+							}
+							
+							int[] effectLengths = {3200, 2500, 2000, 1400};
+							int[] effectLevels = {5, 2, 1, 1};
+							int effectIndex = 0;
+							for(PotionEffectType effectType:Arrays.asList(PotionEffectType.POISON, PotionEffectType.HUNGER, PotionEffectType.CONFUSION, PotionEffectType.BLINDNESS)) {
+								for(PotionEffect effect:activeEffects) {
+									if(effectType.equals(effect.getType())) {
+										effectLengths[effectIndex] += effect.getDuration();
+									}
+								}
+								p.addPotionEffect(new PotionEffect(effectType, effectLengths[effectIndex], effectLevels[effectIndex]));
+								effectIndex++;
+							}
+							
+							p.setMetadata("isPufferfishJustEat", new FixedMetadataValue(DogeFeatures.getPlugin(), false));
+							
+						}
+					}, 180);
 				}
-			}, 180);
+			}, 1);
 			
 			p.setMetadata("timePufferfishLastEat", new FixedMetadataValue(DogeFeatures.getPlugin(), new Date().getTime()));
 			
